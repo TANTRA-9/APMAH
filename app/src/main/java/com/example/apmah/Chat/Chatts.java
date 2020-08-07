a620656ace0267cc18a849b48d389d81910f94cd
Nishant Toamr <nishanttomar00009@gmail.com>
2020-08-07 16:17:09 +0530

    ChatActivity

---
+++ app/src/main/java/com/example/apmah/Chat/Chatts.java
@@ -0,0 +1,18 @@
+package com.example.apmah.Chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apmah.R;
+
+public class Chatts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatts);
+
+        
+    }
+}
